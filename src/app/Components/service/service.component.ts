aced20b40e91ff299f8480916b3b9d3c82a0d415
Shivam Prashar <shivamsharmaprashar@gmail.com>
2021-12-17 11:30:40 +0530

    abc

--- src/app/Components/service/service.component.ts
+++ src/app/Components/service/service.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-service',
   templateUrl: './service.component.html',
   styleUrls: ['./service.component.css']
 })
 export class ServiceComponent implements OnInit {
+  name = 'Get Current Url Route Demo';
+  currentRoute: string;
 
   constructor() { }
 
